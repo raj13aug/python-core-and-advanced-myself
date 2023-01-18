757deee9b305eaa28877b5fdad40e5962beb1da3
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-01-18 20:58:50 +0530

    Create 25-string.py

---
+++ 25-string.py
@@ -0,0 +1,22 @@
+
+# Sting the print
+s = "You are awesome"
+print(s)
+
+
+#String with double quote
+
+s1 = """
+You are the youtuber
+"""
+
+print(s1)
+
+#Multiple the string
+print(s1*2)
+
+# print the index about string also start with zero value
+print(s[3])
+
+# count the string letter
+print(len(s1))
